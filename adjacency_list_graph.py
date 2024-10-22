cb57daddbcdc9a35a40c30467add62d575b64369
suyiiyii <suyiiyii@gmail.com>
2024-10-22 11:57:30 +0800

    更新邻接表图类类型注解

--- adjacency_list_graph.py
+++ adjacency_list_graph.py
@@ -1,15 +1,15 @@
-from typing import Iterator, cast, Callable
+from typing import Iterator, Callable
 
 from graph import Graph, Node, Edge, U, T
 
 
-class AdjacencyListEdge(Edge):
+class AdjacencyListEdge(Edge[U]):
 
-    def __init__(self, from_node: Node, to_node: Node, value: U = None):
+    def __init__(self, from_node: "AdjacencyListNode[T]", to_node: "AdjacencyListNode[T]", value: U = None):
         self._graph: AdjacencyListGraph
-        self._from_node = from_node
-        self._to_node = to_node
-        self._value = value
+        self._from_node: AdjacencyListNode[T] = from_node
+        self._to_node: AdjacencyListNode[T] = to_node
+        self._value: U = value
 
     def __repr__(self):
         return f"Edge({self._from_node}, {self._to_node}, {self._value})"
@@ -19,27 +19,26 @@ class AdjacencyListEdge(Edge):
         return self._value
 
     @property
-    def from_node(self) -> Node:
+    def from_node(self) -> "AdjacencyListNode[T]":
         return self._from_node
 
     @property
-    def to_node(self) -> Node:
+    def to_node(self) -> "AdjacencyListNode[T]":
         return self._to_node
 
     def destroy(self):
         for edge in self._from_node.edges:
             if edge.to_node == self._to_node:
-                node = cast(AdjacencyListNode, self._from_node)
-                node.remove_edge(edge)
+                self._from_node.remove_edge(edge)
                 break
 
 
-class AdjacencyListNode(Node):
+class AdjacencyListNode(Node[T]):
 
     def __init__(self, value: T = None):
         self._graph: AdjacencyListGraph = None
         self._value: T = value
-        self._edge: ["Edge[U]"] = []
+        self._edge: ["AdjacencyListEdge[U]"] = []
 
     def __repr__(self):
         return f"Node({self._value})"
@@ -49,22 +48,22 @@ class AdjacencyListNode(Node):
         return self._value
 
     @property
-    def edges(self) -> Iterator["Edge"]:
+    def edges(self) -> Iterator["AdjacencyListEdge[U]"]:
         return iter(self._edge)
 
-    def add_edge(self, node: "Node", value: U) -> "Edge":
+    def add_edge(self, node: "AdjacencyListNode[T]", value: U) -> "AdjacencyListEdge[U]":
         new_edge = AdjacencyListEdge(self, node, value)
-        new_edge._graph = self._graph.get_edges_func(lambda x:x)
+        new_edge._graph = self._graph.get_edges_func(lambda x: x)
         self._edge.append(new_edge)
         return new_edge
 
-    def remove_edge(self, edge: "Edge"):
+    def remove_edge(self, edge: "AdjacencyListEdge[U]"):
         self._edge.remove(edge)
 
     def destroy(self):
         # 删除其他节点到当前节点的边
-        for egde in self._graph.get_edges_func(lambda x:x.to_node == self):
-            egde.destroy()
+        for edge in self._graph.get_edges_func(lambda x: x.to_node == self):
+            edge.destroy()
         # 删除当前节点到其他节点的边
         for edge in self._edge:
             edge.destroy()
@@ -75,34 +74,34 @@ class AdjacencyListNode(Node):
 class AdjacencyListGraph(Graph):
 
     def __init__(self):
-        self._nodes: ["Node[T]"] = []
+        self._nodes: [AdjacencyListNode[T]] = []
 
     def __repr__(self):
         return f"Graph({self._nodes})"
 
-    def add_node(self, value: T) -> Node:
+    def add_node(self, value: T) -> AdjacencyListNode[T]:
         new_node = AdjacencyListNode(value)
         new_node._graph = self
         self._nodes.append(new_node)
         return new_node
 
-    def get_nodes(self, value: T) -> Iterator[Node]:
+    def get_nodes(self, value: T) -> Iterator[AdjacencyListNode[T]]:
         for node in self._nodes:
             if node.value == value:
                 yield node
 
-    def get_edges(self, value: U) -> Iterator[Edge]:
+    def get_edges(self, value: U) -> Iterator[AdjacencyListEdge[U]]:
         for node in self._nodes:
             for edge in node.edges:
                 if edge.value == value:
                     yield edge
 
-    def get_nodes_func(self, func: Callable[[Node[T]], bool]) -> Iterator[Node]:
+    def get_nodes_func(self, func: Callable[[AdjacencyListNode[T]], bool]) -> Iterator[AdjacencyListNode[T]]:
         for node in self._nodes:
             if func(node):
                 yield node
 
-    def get_edges_func(self, func: Callable[[Edge[U]], bool]) -> Iterator[Edge]:
+    def get_edges_func(self, func: Callable[[AdjacencyListEdge[U]], bool]) -> Iterator[AdjacencyListEdge[U]]:
         for node in self._nodes:
             for edge in node.edges:
                 if func(edge):
