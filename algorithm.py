441109bbd0155e3eb952c58a2f064bf635c83c8c
suyiiyii <suyiiyii@gmail.com>
2024-11-04 15:41:24 +0800

    feat(algorithm): 添加最小生成树算法
    - 实现了 Prim算法和 Kruskal算法
    - 添加了并查集数据结构
    - 更新了 test_algorithm.py，增加了最小生成树算法的测试

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,7 @@ import queue
 from graph import Graph, Node
 
 
-def dijkstra(g: Graph[int, int], start: Node[int], end: Node[int]) -> (int,list[Node[int]]):
+def dijkstra(g: Graph[int, int], start: Node[int], end: Node[int]) -> (int, list[Node[int]]):
     """Dijkstra算法，返回从 start 到 end 的最短路径"""
     pri_queue = queue.PriorityQueue()
     pri_queue.put((0, start, [start]))
@@ -14,18 +14,18 @@ def dijkstra(g: Graph[int, int], start: Node[int], end: Node[int]) -> (int,list[
             continue
         visited.add(node)
         if node == end:
-            return distance,path
+            return distance, path
         for edge in node.edges:
             pri_queue.put((distance + edge.value, edge.to_node, path + [edge.to_node]))
-    return -1,[]
+    return -1, []
 
 
-def topo_sort(g:Graph) -> list:
+def topo_sort(g: Graph) -> list:
     """拓扑排序 返回一个拓扑排序后的节点列表"""
     in_degree = {}
-    for node in g.get_nodes_func(lambda x:x):
+    for node in g.get_nodes_func(lambda x: True):
         in_degree[node] = 0
-    for node in g.get_nodes_func(lambda x:x):
+    for node in g.get_nodes_func(lambda x: True):
         for edge in node.edges:
             in_degree[edge.to_node] += 1
     que = []
@@ -41,3 +41,57 @@ def topo_sort(g:Graph) -> list:
             if in_degree[edge.to_node] == 0:
                 que.append(edge.to_node)
     return ans
+
+
+def prim(g: Graph[any, int]) -> Graph[any, int]:
+    """Prim算法，返回最小生成树"""
+    nodes = list(g.get_nodes_func(lambda x: True))
+    if not nodes:
+        return None
+    new_g = g.__class__()
+    start_node = nodes[0]
+    visited = {start_node}
+    for node in nodes:
+        new_g.add_node(node.value)
+    while len(visited) < len(nodes):
+        min_edge = None
+        for node in visited:
+            for edge in node.edges:
+                if edge.to_node not in visited and (min_edge is None or edge.value < min_edge.value):
+                    min_edge = edge
+        if min_edge is None:
+            break
+        visited.add(min_edge.to_node)
+        from_node = next(new_g.get_nodes(min_edge.from_node.value))
+        to_node = next(new_g.get_nodes(min_edge.to_node.value))
+        from_node.add_edge(to_node, min_edge.value)
+    return new_g
+
+def kruskal(g: Graph[any, int]) -> Graph[any, int]:
+    """Kruskal算法，返回最小生成树"""
+    nodes = list(g.get_nodes_func(lambda x: True))
+    edges = list(g.get_edges_func(lambda x: True))
+    new_g = g.__class__()
+    for node in nodes:
+        new_g.add_node(node.value)
+    # 初始化并查集
+    parent = {}
+    def find(x):
+        if x not in parent:
+            parent[x] = x
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
+    def union(x, y):
+        parent[find(x)] = find(y)
+    def same(x, y):
+        return find(x) == find(y)
+
+    edges.sort(key=lambda x: x.value)
+    for edge in edges:
+        if not same(edge.from_node, edge.to_node):
+            from_node = next(new_g.get_nodes(edge.from_node.value))
+            to_node = next(new_g.get_nodes(edge.to_node.value))
+            from_node.add_edge(to_node, edge.value)
+            union(edge.from_node, edge.to_node)
+    return new_g
--- test_algorithm.py
+++ test_algorithm.py
@@ -1,5 +1,5 @@
 from adjacency_list_graph import AdjacencyListGraph
-from algorithm import dijkstra, topo_sort
+from algorithm import dijkstra, topo_sort, kruskal
 from graph import Graph
 
 
@@ -39,3 +39,62 @@ def test_topo():
     print(ret)
     assert ret == [next(g.get_nodes("A")), next(g.get_nodes("B")), next(g.get_nodes("D")), next(g.get_nodes("C")),
                    next(g.get_nodes("E")), next(g.get_nodes("F")), ]
+
+
+def test_prim():
+    from adjacency_list_graph import AdjacencyListGraph
+    from algorithm import prim
+    from graph import Graph
+    from main import print_graph
+    g: Graph[str, int] = AdjacencyListGraph()
+    g.add_node("A")
+    g.add_node("B")
+    g.add_node("C")
+    g.add_node("D")
+    g.add_node("E")
+
+    edges = ["A B 3",
+             "A D 8",
+             "A E 10",
+             "B D 6",
+             "D E 9",
+             "E C 12",
+             "B C 15"]
+    for edge in edges:
+        edge = edge.split()
+        node1 = next(g.get_nodes(edge[0]))
+        node2 = next(g.get_nodes(edge[1]))
+        node1.add_edge(node2, int(edge[2]))
+        node2.add_edge(node1, int(edge[2]))
+
+    new_g = prim(g)
+    print_graph(new_g)
+
+
+def test_kruskal():
+    from adjacency_list_graph import AdjacencyListGraph
+    from graph import Graph
+    from main import print_graph
+    g: Graph[str, int] = AdjacencyListGraph()
+    g.add_node("A")
+    g.add_node("B")
+    g.add_node("C")
+    g.add_node("D")
+    g.add_node("E")
+
+    edges = ["A B 3",
+             "A D 8",
+             "A E 10",
+             "B D 6",
+             "D E 9",
+             "E C 12",
+             "B C 15"]
+    for edge in edges:
+        edge = edge.split()
+        node1 = next(g.get_nodes(edge[0]))
+        node2 = next(g.get_nodes(edge[1]))
+        node1.add_edge(node2, int(edge[2]))
+        node2.add_edge(node1, int(edge[2]))
+
+    new_g = kruskal(g)
+    print_graph(new_g)
