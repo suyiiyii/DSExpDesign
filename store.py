5cbd18e48111c4e76d3fab2fba8c728e26f7d4b3
suyiiyii <suyiiyii@gmail.com>
2024-12-05 20:25:48 +0800

    refactor(store): 重构数据存储和加载逻辑
    - 将功能函数封装为 Database 类
    -优化数据存储 (_store 方法) 和加载 (_load 方法) 的逻辑- 添加 cities 和 transports 属性，方便外部访问数据
    - 修改 generate_graph 函数，使其成为类内部方法

--- store.py
+++ store.py
@@ -3,34 +3,49 @@ import json
 from models import City, Transport
 
 
-def store(cities: list[City], transports: list[Transport]):
-    with open("city.json", "w", encoding="utf-8") as f:
-        f.write(json.dumps([city.__dict__ for city in cities]))
-    with open("transport.json", "w", encoding="utf-8") as f:
-        f.write(json.dumps([transport.__dict__ for transport in transports]))
-    print("Data stored successfully")
-    return
+class Database:
+
+    def __init__(self):
+        self._cities: list[City] = self._load()[0]
+        self._transports: list[Transport] = self._load()[1]
+        self._generate_graph()
+
+    def _store(self):
+        with open("city.json", "w", encoding="utf-8") as f:
+            f.write(json.dumps([city.__dict__ for city in self._cities]))
+        with open("transport.json", "w", encoding="utf-8") as f:
+            f.write(json.dumps([transport.__dict__ for transport in self._transports]))
+        print("Data stored successfully")
+        return
+
+    def _generate_graph(self):
+        with open("graph.md", "w", encoding="utf-8") as f:
+            f.write("# 线路图\n")
+            f.write("```mermaid\n")
+            f.write("graph\n")
+            '''深圳 -- G2944 --> 广州'''
+            for transport in self._transports:
+                f.write(f"\t{transport.start} -- {transport.run_id} --> {transport.end}\n")
+            f.write("```")
+
+    def _load(self) -> (list[City], list[Transport]):
+        with open("city.json", "r", encoding="utf-8") as f:
+            cities = [City(**city) for city in json.loads(f.read())]
+        with open("transport.json", "r", encoding="utf-8") as f:
+            transports = [Transport(**transport) for transport in json.loads(f.read())]
+        return cities, transports
+
+    @property
+    def cities(self) -> list[City]:
+        return self._cities
+
+    @property
+    def transports(self) -> list[Transport]:
+        return self._transports
 
 
-def generate_graph(transports: list[Transport]):
-    with open("graph.md", "w", encoding="utf-8") as f:
-        f.write("# 线路图\n")
-        f.write("```mermaid\n")
-        f.write("graph\n")
-        '''深圳 -- G2944 --> 广州'''
-        for transport in transports:
-            f.write(f"\t{transport.start} -- {transport.run_id} --> {transport.end}\n")
-        f.write("```")
-
-
-def load() -> (list[City], list[Transport]):
-    with open("city.json", "r", encoding="utf-8") as f:
-        cities = [City(**city) for city in json.loads(f.read())]
-    with open("transport.json", "r", encoding="utf-8") as f:
-        transports = [Transport(**transport) for transport in json.loads(f.read())]
-    generate_graph(transports)
-    return cities, transports
-
+db = Database()
 
 if __name__ == '__main__':
-    print(load())
+    print(db.cities)
+    print(db.transports)
