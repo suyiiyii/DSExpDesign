7f4b69aa268fbb317c3c14c9ed55eacf30d8b2a8
suyiiyii <suyiiyii@gmail.com>
2024-12-05 20:52:07 +0800

    feat: 添加 FastAPI 接口和相关依赖
    - 新增 api.py 文件，实现城市和交通方式的 GET 接口
    - 添加 requirements.txt 文件，列出项目所需的 Python 依赖
    - 主要依赖包括 FastAPI、Pydantic 和 uvicorn 等

---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+import models
+from store import db
+
+app = FastAPI()
+
+
+@app.get("/city", response_model=list[models.City])
+async def get_cities():
+    return db.cities
+
+
+@app.get("/transport", response_model=list[models.Transport])
+async def get_transports():
+    return db.transports
