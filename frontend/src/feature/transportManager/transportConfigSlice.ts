18c893bc8e4a9ab7187a08b7392dabf74cdef9fa
suyiiyii <suyiiyii@gmail.com>
2024-12-07 00:13:38 +0800

    refactor(frontend): 调整运输经理功能模块位置
    - 将 transportManager 目录从 frontend/src 移动到 frontend/src/feature/
    - 更新 transportConfigSlice.ts 文件中的导入路径

--- frontend/src/feature/transportManager/transportConfigSlice.ts
+++ frontend/src/feature/transportManager/transportConfigSlice.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {Transport} from "../utils/types";
+import {Transport} from "../../utils/types";
 
 
 export const transportConfigSlice = createApi({
