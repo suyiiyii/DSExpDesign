761792d98c51ba2e0bef5eb0e8b1f34207e8aae8
suyiiyii <suyiiyii@gmail.com>
2024-12-10 19:43:11 +0800

    feat(frontend): 优化运输列表渲染性能
    - 引入 react-window 库以提高列表渲染效率
    - 用 FixedSizeList组件替换原有的 List 组件
    - 通过 row 函数动态渲染列表项，减少一次性渲染的组件数量
    - 调整列表样式和布局以适应新实现

--- frontend/src/feature/transportManager/TransportList.tsx
+++ frontend/src/feature/transportManager/TransportList.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import TrainIcon from "@mui/icons-material/Train";
 import FlightIcon from "@mui/icons-material/Flight";
+import {FixedSizeList, ListChildComponentProps} from "react-window";
 
 const style = {
     py: 0,
@@ -19,35 +20,49 @@ export function TransportList({transports, doDelete}: {
     transports: Transport[],
     doDelete?: (transport: Transport) => void
 }) {
+
+    function row(props: ListChildComponentProps) {
+        const {index} = props;
+        const transport = transports[index];
+        return (
+            <React.Fragment key={JSON.stringify(transport)}>
+                <ListItem
+                    secondaryAction={
+                        doDelete && (
+                            <IconButton edge="end" aria-label="delete" onClick={() => {
+                                doDelete(transport)
+                            }}>
+                                <DeleteIcon/>
+                            </IconButton>)
+                    }
+                >
+                    <ListItemIcon>
+                        {transport.type === 'train' ? <TrainIcon/> : <FlightIcon/>}
+                    </ListItemIcon>
+                    <div>
+                        <ListItemText primary={`${transport.start} --${transport.name}-> ${transport.end}`}/>
+                        <p>
+                            {`${transport.start_time} - ${transport.end_time}  ${transport.price}￥`}
+                        </p>
+                    </div>
+                </ListItem>
+                {index !== transports.length - 1 && (
+                    <Divider component="li"/>
+                )}
+            </React.Fragment>
+        );
+    }
+
     return (
         <List sx={style}>
-            {transports.map((transport, index) => (
-                <React.Fragment key={JSON.stringify(transport)}>
-                    <ListItem
-                        secondaryAction={
-                            doDelete && (
-                                <IconButton edge="end" aria-label="delete" onClick={() => {
-                                    doDelete(transport)
-                                }}>
-                                    <DeleteIcon/>
-                                </IconButton>)
-                        }
-                    >
-                        <ListItemIcon>
-                            {transport.type === 'train' ? <TrainIcon/> : <FlightIcon/>}
-                        </ListItemIcon>
-                        <div>
-                            <ListItemText primary={`${transport.start} --${transport.name}-> ${transport.end}`}/>
-                            <p>
-                                {`${transport.start_time} - ${transport.end_time}  ${transport.price}￥`}
-                            </p>
-                        </div>
-                    </ListItem>
-                    {index !== transports.length - 1 && (
-                        <Divider component="li"/>
-                    )}
-                </React.Fragment>
-            ))}
+            <FixedSizeList
+                itemSize={100}
+                height={400}
+                itemCount={transports.length}
+                width={360}
+                overscanCount={5}>
+                {row}
+            </FixedSizeList>
         </List>
     );
 }
