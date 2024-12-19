3b706d20585450c8e635b6017b13104da25b66f6
suyiiyii <suyiiyii@gmail.com>
2024-12-19 12:59:41 +0800

    fix(routeMap): 优化 RouteMapView 组件以解决渲染问题
    - 添加 sha1 函数以生成 chart 数据的哈希值
    - 使用哈希值作为 key，避免重复渲染导致的显示问题
    -优化组件状态管理，提高渲染效率

--- frontend/src/feature/routeMap/RouteMapView.tsx
+++ frontend/src/feature/routeMap/RouteMapView.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import MermaidChart from "../../utils/mermaid/MermaidChart";
 import {useGetRouteMapQuery} from "./routeMapSlice";
 
+async function sha1(message: string): Promise<string> {
+    // Encode the message as an ArrayBuffer
+    const msgBuffer = new TextEncoder().encode(message);
+
+    // Calculate the SHA-1 hash
+    const hashBuffer = await crypto.subtle.digest('SHA-1', msgBuffer);
+
+    // Convert the ArrayBuffer to a hexadecimal string
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+    return hashHex;
+}
+
 export default function RouteMapView() {
-    const chart = useGetRouteMapQuery().data || {data: ""};
-    console.log(chart)
+    const {data: chartData} = useGetRouteMapQuery();
+    const [chart, setChart] = useState({data: "", id: ""});
+
+    useEffect(() => {
+        if (chartData) {
+            sha1(chartData.data).then(hash => {
+                setChart({data: chartData.data, id: hash});
+            });
+        }
+    }, [chartData]);
+    // 使用 data 的哈希作为 key，避免相同位置重复渲染导致的 mermaid 无法正常显示的问题
+
     return (
         <div>
             <h1>Route Map</h1>
             {chart.data === "" ? (
                 <div>loading...</div>
             ) : (
-                <MermaidChart chart={`${chart.data}`}></MermaidChart>
+                <Fragment key={chart.id}>
+                    <MermaidChart chart={chart.data}/>
+                </Fragment>
             )}
         </div>
-    )
+    );
 }
